69a18359a4c5e48e4c9a840ea90dafc3e5ba69e0
viswanadh800 <viswanadh.800@gmail.com>
2024-02-09 14:05:43 +0530

    1st commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String args[]){
+        RWOnly obj=new RWOnly();
+        /*
+        obj.name = "HelloWorld";
+        It is error because name is private member of RWOnly class.
+         */
+        obj.setName("Hello World");
+        System.out.println(obj.getName());
+    }
 }
